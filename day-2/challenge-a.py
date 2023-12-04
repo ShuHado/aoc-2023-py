c59c0e38df4386284bc7eb46df38dc38530ea89a
Cyril Arrachequesne <cyrilou564@gmail.com>
2023-12-04 23:46:18 +0100

    ♻️ add function run

--- day-2/challenge-a.py
+++ day-2/challenge-a.py
@@ -2,31 +2,31 @@ file = open("day-2/input.txt", "r")
 input = file.readlines()
 file.close()
 
-total = 0
-
 redMax = 12
 greenMax = 13
 blueMax = 14
 
 colors = ["red", "green", "blue"]
 
-for line in input:
-    line = line.strip()
-    line = line.split(":")
-    line[0] = line[0].split(" ")
-    line[1] = line[1].split(";")
-    print(line)
-    isPossible = True
-    for set in line[1]:
-        set = set.split(",")
-        for entry in set:
-            entry = entry.strip()
-            entry = entry.split(" ")
-            for color in colors:
-                if color in entry:
-                    if int(entry[0]) > eval(f"{color}Max"):
-                        isPossible = False
-    if isPossible:
-        total += int(line[0][1])
 
-print(f"Challenge A \U0001F385 : {total}")
+def run():
+    total = 0
+    for line in input:
+        line = line.strip()
+        line = line.split(":")
+        line[0] = line[0].split(" ")
+        line[1] = line[1].split(";")
+        print(line)
+        isPossible = True
+        for set in line[1]:
+            set = set.split(",")
+            for entry in set:
+                entry = entry.strip()
+                entry = entry.split(" ")
+                for color in colors:
+                    if color in entry:
+                        if int(entry[0]) > eval(f"{color}Max"):
+                            isPossible = False
+        if isPossible:
+            total += int(line[0][1])
+    return total
--- day-2/challenge-b.py
+++ day-2/challenge-b.py
@@ -2,28 +2,26 @@ file = open("day-2/input.txt", "r")
 input = file.readlines()
 file.close()
 
-total = 0
-
 colors = ["red", "green", "blue"]
 
-for line in input:
-    redMax = 0
-    greenMax = 0
-    blueMax = 0
-    line = line.strip()
-    line = line.split(":")
-    line[1] = line[1].split(";")
-    line = line[1]
-    for set in line:
-        set = set.split(",")
-        for entry in set:
-            entry = entry.strip()
-            entry = entry.split(" ")
-            value = int(entry[0])
-            entry_color = entry[1]
-            for color in colors:
-                if color == entry_color:
-                    globals()[color + "Max"] = max(globals()[color + "Max"], value)
-    total += redMax * greenMax * blueMax
 
-print(f"Challenge B \U0001F385 : {total}")
+def run():
+    total = 0
+    for line in input:
+        colorMax = {"red": 0, "green": 0, "blue": 0}
+        line = line.strip()
+        line = line.split(":")
+        line[1] = line[1].split(";")
+        line = line[1]
+        for set in line:
+            set = set.split(",")
+            for entry in set:
+                entry = entry.strip()
+                entry = entry.split(" ")
+                value = int(entry[0])
+                entry_color = entry[1]
+                for color in colors:
+                    if color == entry_color:
+                        colorMax[color] = max(colorMax[color], value)
+        total += colorMax["red"] * colorMax["green"] * colorMax["blue"]
+    return total
